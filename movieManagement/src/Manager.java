1081299e7f957a563f4ff0488c7fe35ad3df1eed
shayne773 <111240364+shayne773@users.noreply.github.com>
2022-12-06 14:21:42 -0500

    Add files via upload

--- movieManagement/src/Manager.java
+++ movieManagement/src/Manager.java
@@ -1,4 +1,35 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Manager {
+	
+	
+	String id;
+	String password;
+	
+	Manager()
+	{
+		
+	}
+	
+	Manager(String id, String password)
+	{
+		this.id = id;
+		this.password=password;
+		try
+		{
+			FileWriter writer = new FileWriter("manager_data.txt", true);
+			writer.append("\n"+this.id+", "+this.password);
+			writer.close();
+			
+			
+			
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 
 }
